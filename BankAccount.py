71c634210c4efe491c86d39b23ed90ef5e9996a8
johnnyquinn <jftq97@gmail.com>
2020-11-08 21:00:04 -0500

    wrote add_interest() and print_receipt()

--- BankAccount.py
+++ BankAccount.py
@@ -5,11 +5,13 @@ class BankAccount():
         self.routing_number = 0
         self.balance = 0
     
+    #add amount to balance
     def deposit(self, amount):
         self.balance += amount 
         print(f'Amount Deposited: ${amount}')
-        get_balance(0)
+        get_balance()
 
+    #subtracts amount from balance
     def withdraw(self, amount):
         if self.balance < amount 
             print(f'Insufficient funds.\n Overdraft fee: $10')
@@ -20,7 +22,16 @@ class BankAccount():
 
         get_balance()
 
+    #print current balance and returns amount
     def get_balance(self):
         print(f'Your current balance is {self.balance}')  
         return self.balance   
 
+    #calculates interest, 1%/yr
+    def add_interest(self):
+        interest = self.balance * 0.00083
+
+    def print_receipt(self):
+        print full_name
+        print(f'Account No.: ****{account_number[-5]}')
+        print(f'Routing No.: {routing_number}')

6d77a020acd4431165ce5207fdbba1a36b9e6c4f
johnnyquinn <jftq97@gmail.com>
2020-11-08 21:13:51 -0500

    created johnny object

--- BankAccount.py
+++ BankAccount.py
@@ -1,37 +1,45 @@
 class BankAccount():
-    def __init__(self, full_name, account_number)
+    def __init__(self, full_name, account_number):
         self.full_name = full_name
         self.account_number = account_number
-        self.routing_number = 0
+        self.routing_number = 546325981
         self.balance = 0
     
     #add amount to balance
     def deposit(self, amount):
         self.balance += amount 
         print(f'Amount Deposited: ${amount}')
-        get_balance()
+        self.get_balance()
 
     #subtracts amount from balance
     def withdraw(self, amount):
-        if self.balance < amount 
+        if self.balance < amount: 
             print(f'Insufficient funds.\n Overdraft fee: $10')
             self.balance -= 10
-        else 
+        else: 
             self.balance +- amount
             print(f'Amount Withdrawn: ${amount}')
 
-        get_balance()
+        self.get_balance()
 
     #print current balance and returns amount
     def get_balance(self):
-        print(f'Your current balance is {self.balance}')  
-        return self.balance   
+        return f'${self.balance}'
 
     #calculates interest, 1%/yr
     def add_interest(self):
-        interest = self.balance * 0.00083
+        interest = round(self.balance * 0.00083, 2)
+        self.balance += interest
 
     def print_receipt(self):
-        print full_name
-        print(f'Account No.: ****{account_number[-5]}')
-        print(f'Routing No.: {routing_number}')
+        account_num_str = (str(self.account_number))[-5:]
+        print("--------------------------------------------")
+        print(self.full_name)
+        print(f'Account No.: ****{account_num_str}')
+        print(f'Routing No.: {self.routing_number}')
+        print(f'Balance: {self.get_balance()}')
+
+johnny = BankAccount('Johnny Quinn', 5465231)
+johnny.deposit(150)
+johnny.add_interest()
+johnny.print_receipt()

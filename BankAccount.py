23049d9c100a2479ef53d7df616e9b7e1c85482e
johnnyquinn <jftq97@gmail.com>
2020-11-10 20:37:55 -0500

    added README.md and comments

--- BankAccount.py
+++ BankAccount.py
@@ -3,8 +3,8 @@ import random
 class BankAccount():
     def __init__(self, full_name):
         self.full_name = full_name
-        self.account_number = random.randint(10000000, 99999999)
-        self.routing_number = 546325981
+        self.account_number = random.randint(10000000, 99999999) # Randomly generated 8 digit account number
+        self.routing_number = 546325981 # standardized routing number
         self.balance = 0
     
     #add amount to balance
@@ -24,7 +24,7 @@ class BankAccount():
 
         self.get_balance()
 
-    #print current balance and returns amount
+    #prints current balance
     def get_balance(self):
         print(f'Balance: ${round(self.balance, 2)}')
 
@@ -35,8 +35,9 @@ class BankAccount():
             self.balance += interest
         print(f'- Interest: 1% per year -\nTotal months of accrued interest: {str(months)}')
         
+    #prints name, account number, routing number, and balance related to the account
     def print_receipt(self):
-        account_num_str = (str(self.account_number))[-4:]
+        account_num_str = (str(self.account_number))[-4:] # only reveals last 4 numbers of the account number
         print("--------------------------------------------")
         print(self.full_name)
         print(f'Account No.: ****{account_num_str}')
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bank Account 
+
+Using python, this program simulates operations of bank accounts. Bank accounts are objects under the bank account class and have methods such as deposit, withdraw, get balance, add interest, and print reciept. 
